db50b1ecac141a2ca43b7f4f1b58d7736ea85558
Egor Demeshko <demeshko.egor@gmail.com>
2023-11-29 14:00:12 +0300

    post page

---
+++ assets/modules/topBar.js
@@ -0,0 +1,26 @@
+(function controllscroll(){
+    const element = document.querySelector('.header');
+
+    function controllTopScroll(){
+        if(window.scrollY > 0){
+            element.classList.add('header--dark');
+        } else {
+            element.classList.remove('header--dark');
+        }
+    }
+
+    const freezed = (() => {
+        let freezed = false;
+        return () => {
+            if(freezed) return;
+            freezed = true;
+ 
+            setTimeout( () => {
+                freezed = false;
+                controllTopScroll();
+            }, 100);
+        }
+    })();
+
+    document.addEventListener("scroll", freezed);
+})();
--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 import HeroSlider from "/assets/modules/HeroSlider.js";
 import AnimatedLinks from "/assets/modules/AnimatedLinks.js";
 import "/assets/modules/menuButton.js";
+import "/assets/modules/goUp.js";
+import "/assets/modules/topBar.js";
 
 export const slider = new HeroSlider();
 export const animatedLinks = new AnimatedLinks({elementsClass: ".links"});
